854fb7838ad29f7dfab7124bcf25c85ff60174d6
rohit kumar sah <rohit.sah892@atmecs.com>
2022-06-02 23:31:31 +0530

    testimonial section completed 50%

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import './testimonials.scss'
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 
 function Testimonials() {
   return (
     <div className='testimonials' id="testimonials">
-      
+      <div className="slider">
+        <div className="container">
+          <div className="card">
+          <div className="left">
+            
+             <div className="imagecontainer">
+             </div>
+            
+          </div>
+          <div className="right">
+            <div className="rightcontainer">
+              <div className="top">
+              <h2>Client Reviews</h2>
+              </div>
+              <div className="bottom">
+                <div className="bottomtop">
+                <FormatQuoteIcon className='leftquote'/>
+                </div>
+              <div className="bottomcenter">
+              <span className='quote'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, commodi tempora velit nihil nisi quisquam qui magni accusantium mollitia et.</span>
+              </div>
+              <div className="bottombottom">
+              <FormatQuoteIcon className='rightquote'/>
+              </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
