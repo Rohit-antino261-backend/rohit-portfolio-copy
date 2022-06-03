46c0e80a052a9040d0b049d30ca97006a092c042
rohit kumar sah <rohit.sah892@atmecs.com>
2022-06-03 08:27:43 +0530

    testimonial slide working now

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './testimonials.scss'
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import AdjustIcon from '@mui/icons-material/Adjust';
+import FiberManualRecordOutlinedIcon from '@mui/icons-material/FiberManualRecordOutlined';
+import { useSelect } from '@mui/base';
+
+const testimonials=[
+  {
+    id:1,
+    img:"./assets/testimonial-user-two.png",
+    review:"Review one",
+    name:"Satish Patel",
+    position:""
+  },
+  {
+    id:2,
+    img:"./assets/testimonial-user-three.png",
+    review:"Review two",
+    name:"Satish Patel",
+    position:""
+  },
+  {
+    id:3,
+    img:"./assets/testimonial-user-four.jpg",
+    review:"Review three",
+    name:"Satish Patel",
+    position:""
+  },
+  {
+    id:4,
+    img:"./assets/testimonial-user-five.jpg",
+    review:"Review four",
+    name:"Satish Patel",
+    position:""
+  }
+  
+]
 
 function Testimonials() {
+  const [activetestimonial,setActiveTestimonial]=useState(0)
+  const [activeToggle,setActiveToggle]=useState(0)
+
+const handleTestimonial=(id)=>{
+   setActiveToggle(id)
+   setActiveTestimonial(id)
+}
+console.log(activetestimonial)
   return (
     <div className='testimonials' id="testimonials">
-      <div className="slider">
-        <div className="container">
+      <div className="slider" style={{transform: `translateX(${activetestimonial*100}vw)`}}>
+
+        {testimonials.map((item)=>(
+          <div className="container">
           <div className="card">
           <div className="left">
             
@@ -34,6 +79,14 @@ function Testimonials() {
           </div>
           </div>
         </div>
+        ))}
+
+      </div>
+      <div className="navigate">
+        <FiberManualRecordOutlinedIcon className={activeToggle===0? "navigateone active": "navigateone"} onClick={()=>handleTestimonial(0)}/>
+        <FiberManualRecordOutlinedIcon className={activeToggle===1? "navigatetwo active": "navigateone"} onClick={()=>handleTestimonial(1)}/>
+        <FiberManualRecordOutlinedIcon className={activeToggle===2? "navigatethree active": "navigatethree"} onClick={()=>handleTestimonial(2)}/>
+        <FiberManualRecordOutlinedIcon className={activeToggle===3? "navigatefour active": "navigatefour"} onClick={()=>handleTestimonial(3)}/>
       </div>
     </div>
   )
