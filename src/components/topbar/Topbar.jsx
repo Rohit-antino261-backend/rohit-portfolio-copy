1f17de24687a86d73161595395013c633edd729d
rohit kumar sah <rohit.sah892@atmecs.com>
2022-06-05 01:52:46 +0530

    view resume added to topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import './topbar.scss'
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
+import PreviewIcon from '@mui/icons-material/Preview';
 
 function Topbar({viewMenu,setViewMenu}) {
+  const resumeUrl="https://firebasestorage.googleapis.com/v0/b/portfolio-e8625.appspot.com/o/Rohit's%20Resume%20(1).pdf?alt=media&token=15948f2c-412a-4821-a070-e3e63fb7ff54"
   return (
     <div className={viewMenu? 'topbar active':'topbar'}>
         <div className='left'>
@@ -12,6 +14,10 @@ function Topbar({viewMenu,setViewMenu}) {
           <h1 className='email'><MarkEmailReadIcon className='email_logo'/> rohitsah892@gmail.com</h1>
         </div>
         <div className="right">
+          <div className= {viewMenu?"resume active":"resume"}>
+            <PreviewIcon className='viewlogo'/>
+            <a href={resumeUrl}>Resume</a>
+          </div>
           <div className={viewMenu? "hamburger active":"hamburger"} onClick={()=>setViewMenu(!viewMenu)} >
              <span className='hamburger_one' />
              <span className='hamburger_two' />
